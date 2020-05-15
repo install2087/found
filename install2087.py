4caf86957fda80fc847d48b9c4f22071c3095578
install2087 <64840782+install2087@users.noreply.github.com>
2020-05-15 06:22:52 -0700

    Update install2087.py

--- install2087.py
+++ install2087.py
@@ -1,6 +1,6 @@
 a1=input('جملت؟')
 a2=input('کاراکتر هایی که میخوای پیدا کنی بگو و راحت باش مثلا اگه دنبال i و n هستی بنویس i n من برات میگم هر کاراکتر چند بار امده؟ ')
-a=list(a2)
+a=tuple(a2)
 b={}
 for p in a1:
     if p in a:
@@ -8,6 +8,6 @@ for p in a1:
         b[p]+=1
 for b,v in b.items():
     print(b,'=',v)
-print('اپدیت ۲')
+print('اپدیت ۳')
 
 print('@install2087/inastall2087.xyz')
